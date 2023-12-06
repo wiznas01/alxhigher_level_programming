357e1c5eb5bd8076af996211afde68f1a2d88fbf
wiznas01 <wiznas666@gmail.com>
2023-12-06 06:07:17 -0800

    infinte addition

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    args = sys.argv[1:]
+    argc = len(args)
+
+    if argc == 0:
+        print("0")
+    else:
+        result = sum(int(arg) for arg in args)
+        print(result)
