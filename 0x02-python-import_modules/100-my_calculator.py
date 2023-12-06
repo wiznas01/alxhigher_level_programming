b0c341bf2d1fd934de2214b384c165736608a912
wiznas01 <wiznas666@gmail.com>
2023-12-06 06:47:17 -0800

    build calcultor

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+import sys
+
+if len(sys.argv) != 4:
+    print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+    sys.exit(1)
+
+a = int(sys.argv[1])
+operator = sys.argv[2]
+b = int(sys.argv[3])
+
+if operator == '+':
+    result = a + b
+elif operator == '-':
+    result = a - b
+elif operator == '*':
+    result = a * b
+elif operator == '/':
+    result = a / b
+else:
+    print("Unknown operator. Available operators: +, -, * and /")
+    sys.exit(1)
+
+print("{} {} {} = {}".format(a, operator, b, result))
+sys.exit(0)
