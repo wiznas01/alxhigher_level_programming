0e2532bbf30cd2ec35249c9fd1e789579eb0d39c
wiznas01 <wiznas666@gmail.com>
2023-12-06 05:47:44 -0800

    update

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-
+from calculator_1 import add, sub, mul, div
 a = 10
 b = 5
-from calculator_1 import add, sub, mul, div
 
 if __name__ == "__main__":
     result_add = add(a, b)
